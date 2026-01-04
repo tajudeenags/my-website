1d9395afa6e1804c9173dd76e4da6bb3f4a2ee08
tajudeenags <tajudeenags@gmail.com>
2026-01-04 13:59:55 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function showMessage() {
-    alert("Welcome! You clicked the button 🎉");
-}
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar ul li a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop - 150) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
