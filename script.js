18d050a9b0b03f8d268119f60ecf3d84be93f8ce
tajudeenags <tajudeenags@gmail.com>
2026-01-04 14:59:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
 const navLinks = document.querySelectorAll(".navbar ul li a");
-const sections = document.querySelectorAll("section[id]");
 
-const observer = new IntersectionObserver(
-    (entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                navLinks.forEach(link => link.classList.remove("active"));
-                const activeLink = document.querySelector(
-                    `.navbar a[href="#${entry.target.id}"]`
-                );
-                if (activeLink) activeLink.classList.add("active");
-            }
-        });
-    },
-    {
-        root: null,
-        threshold: 0.6
-    }
-);
-
-sections.forEach(section => observer.observe(section));
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.forEach(l => l.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
